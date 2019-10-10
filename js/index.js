d3a2949a26efadeb76f30abd593c2f8791891f3b
kousuke1 <barusa_hamakou@yahoo.co.jp>
2019-10-11 04:14:48 +0900

    10/11

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+
+
+
 $(function(){
 
 
@@ -20,6 +23,7 @@ function changePage(){
 };
 
 //（６）～秒間隔でイメージ切換の発火設定
+    
 var Timer;
 function startTimer(){
 Timer =setInterval(function(){
@@ -43,3 +47,23 @@ startTimer();
 /*オプションを足す場合はここへ記載*/
 
 });
+
+　
+$(function($){
+ 
+  //コンテンツを囲む要素をidで指定
+  var container = document.querySelector('#gallery');
+ 
+  //すべての画像を読み込み終わった後に関数を実行
+  imagesLoaded(container, function () {
+    var msnry = new Masonry(container, {
+      itemSelector: '.item', //コンテンツのclass名
+      isFitWidth: true, //コンテナの親要素のサイズに基づいて、コンテンツのカラムを自動調節します。
+      columnWidth: 310, //カラムの幅を設定
+    });
+  });
+});
+
+
+
+  
